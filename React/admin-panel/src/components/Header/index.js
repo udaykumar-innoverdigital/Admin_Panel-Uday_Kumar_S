e460f29977d42169546b53943ffad5b942b70cc5
udaykumar-innoverdigital <uday.kumar@innoverdigital.com>
2024-06-13 14:00:14 +0530

    added notifications dropdown

--- React/admin-panel/src/components/Header/index.js
+++ React/admin-panel/src/components/Header/index.js
@@ -24,8 +24,11 @@ import { MyContext } from "../../App";
 const Header = () => {
     const [anchorEl, setAnchorEl] = React.useState(null);
     const [isOpenNotificationDrop, setIsOpenNotificationsDrop] = React.useState(false);
+    const [isOpenMailDrop, setIsOpenMailDrop] =React.useState(false);
+
     const openMyAcc = Boolean(anchorEl);
     const openMyNotifications = Boolean(isOpenNotificationDrop);
+    const openMyMail = Boolean(isOpenMailDrop);
 
     const context = useContext(MyContext);
 
@@ -40,6 +43,12 @@ const Header = () => {
     };
     const handleCloseNotificationsDrop = () =>{
         setIsOpenNotificationsDrop(false);
+    };
+    const handleOpenMailDrop = () =>{
+        setIsOpenMailDrop(true);
+    };
+    const handleCloseMailDrop = () =>{
+        setIsOpenMailDrop(false);
     };
     return(
         <>
@@ -63,8 +72,116 @@ const Header = () => {
                         <div className="col-sm-7 d-flex align-items-center justify-content-end part3">
                             <Button className="rounded-circle mr-3"><MdOutlineLightMode/></Button>
                             <Button className="rounded-circle mr-3"><PiShoppingBagBold/></Button>
-                            <Button className="rounded-circle mr-3"><MdOutlineMailOutline/></Button>
-                            
+                            <div className="dropdownWrapper position-relative">
+                                <Button className="rounded-circle mr-3" onClick={handleOpenMailDrop}><MdOutlineMailOutline/></Button>
+                                <Menu
+                                        anchorEl={isOpenNotificationDrop}
+                                        className="notifications dropdown_list"
+                                        id="notifications"
+                                        open={openMyMail}
+                                        onClose={handleCloseMailDrop}
+                                        onClick={handleCloseMailDrop}
+                                        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                                        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                                    >
+                                        <div className="head ps-3 pl-0">
+                                            <h4>Inbox (4)</h4>
+                                        </div>
+
+                                        <Divider style={{ background: 'black' }} variant="middle" />
+                                        
+                                        <div className="scroll">
+                                            <MenuItem onClick={handleCloseMailDrop}>
+                                                <div className="d-flex">
+                                                <div>
+                                                    <div className="userImg">
+                                                        <span className="rounded-circle">
+                                                            <img src = "https://th.bing.com/th/id/R.fba6ecd5d2f7c7925177bceb222b09b1?rik=Ghf2DCU4j3SiZQ&riu=http%3a%2f%2fmedia.techeblog.com%2fimages%2fjoaquin-phoenix-joker.jpg&ehk=bl0nW7Dbn5gDa4Dm1mQ1oRYsz38uRyTfPAdVcWu6GvQ%3d&risl=&pid=ImgRaw&r=0"/>
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                                <div className="dropDownInfo">
+                                                    <h4>
+                                                        <span>
+                                                            <b>Joker</b>
+                                                            Submission of Record
+                                                            <b> Last date to submit record</b>
+                                                        </span>
+                                                    </h4>
+                                                    <p className="text-sky">few seconds ago</p>
+                                                </div>
+                                                </div>
+                                            </MenuItem>
+
+                                            <MenuItem onClick={handleCloseMailDrop}>
+                                                <div className="d-flex">
+                                                <div>
+                                                    <div className="userImg">
+                                                        <span className="rounded-circle">
+                                                            <img src = "https://th.bing.com/th/id/R.fba6ecd5d2f7c7925177bceb222b09b1?rik=Ghf2DCU4j3SiZQ&riu=http%3a%2f%2fmedia.techeblog.com%2fimages%2fjoaquin-phoenix-joker.jpg&ehk=bl0nW7Dbn5gDa4Dm1mQ1oRYsz38uRyTfPAdVcWu6GvQ%3d&risl=&pid=ImgRaw&r=0"/>
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                                <div className="dropDownInfo">
+                                                    <h4>
+                                                        <span>
+                                                            <b>Joker</b>
+                                                            Assigned a task
+                                                            <b> Kill Justice League</b>
+                                                        </span>
+                                                    </h4>
+                                                    <p className="text-sky">few seconds ago</p>
+                                                </div>
+                                                </div>
+                                            </MenuItem>
+                                            <MenuItem onClick={handleCloseMailDrop}>
+                                                <div className="d-flex">
+                                                <div>
+                                                    <div className="userImg">
+                                                        <span className="rounded-circle">
+                                                            <img src = "https://wallpapercave.com/wp/wp8354498.jpg"/>
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                                <div className="dropDownInfo">
+                                                    <h4>
+                                                        <span>
+                                                            <b>Batman</b>
+                                                            Searching Joker
+                                                            <b> Do you know the whereabouts of joker?</b>
+                                                        </span>
+                                                    </h4>
+                                                    <p className="text-sky">few seconds ago</p>
+                                                </div>
+                                                </div>
+                                            </MenuItem>
+                                            <MenuItem onClick={handleCloseMailDrop}>
+                                                <div className="d-flex">
+                                                <div>
+                                                    <div className="userImg">
+                                                        <span className="rounded-circle">
+                                                            <img src = "https://wallpapercave.com/wp/wp8354498.jpg"/>
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                                <div className="dropDownInfo">
+                                                    <h4>
+                                                        <span>
+                                                            <b>Batman</b>
+                                                            Search for Joker
+                                                            <b> Batman is on pursuit to find joker</b>
+                                                        </span>
+                                                    </h4>
+                                                    <p className="text-sky">One day ago</p>
+                                                </div>
+                                                </div>
+                                            </MenuItem>
+                                        </div>
+                                        <div className="ps-2 pe-2 w-100 pt-2 pb-1">
+                                            <Button className="btn-blue w-100">View all notifications</Button>
+                                        </div>
+                                    </Menu>
+                            </div>
                             <div className="dropdownWrapper position-relative">
                                 <Button className="rounded-circle mr-3" onClick={handleOpenNotificationsDrop}><FaRegBell/></Button>
                                 <Menu
@@ -84,7 +201,7 @@ const Header = () => {
                                         <Divider style={{ background: 'black' }} variant="middle" />
                                         
                                         <div className="scroll">
-                                            <MenuItem onClick={handleCloseMyAccDrop}>
+                                            <MenuItem onClick={handleCloseNotificationsDrop}>
                                                 <div className="d-flex">
                                                 <div>
                                                     <div className="userImg">
@@ -106,7 +223,7 @@ const Header = () => {
                                                 </div>
                                             </MenuItem>
 
-                                            <MenuItem onClick={handleCloseMyAccDrop}>
+                                            <MenuItem onClick={handleCloseNotificationsDrop}>
                                                 <div className="d-flex">
                                                 <div>
                                                     <div className="userImg">
